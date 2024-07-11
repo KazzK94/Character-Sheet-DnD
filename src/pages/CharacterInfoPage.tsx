b69a1a680256a49d2c2ef47fe5f59516d55aa627
Kazz <i94773007@gmail.com>
2024-07-12 01:43:26 +0200

    added features to char info

--- src/pages/CharacterInfoPage.tsx
+++ src/pages/CharacterInfoPage.tsx
@@ -18,7 +18,9 @@ export default function CharacterInfoPage() {
 		<div className='mt-3'>
 
 			<div className='flex justify-end mr-4 mb-3'>
-				<select name="section" id="section" onChange={onSelectChange} className='text-black text-lg px-3 py-2'>
+				<select name="section" id="section" onChange={onSelectChange}
+					className='text-lg bg-primary border border-gray-200 px-3 py-2 rounded shadow-lg'
+				>
 					<option value="basicInfo">Información Básica</option>
 					<option value="background">Trasfondo</option>
 					<option value="traits">Rasgos y Características</option>
@@ -54,17 +56,39 @@ export default function CharacterInfoPage() {
 							<CardSubtitle>Trasfondo</CardSubtitle>
 						</CardHeader>
 						<CardContent>
-							<div className='mb-2'>
-								<p>RASGOS DE PERSONALIDAD: Nadie sufrirá mientras yo pueda evitarlo.</p>
-							</div>
-							<div className='mb-2'>
-								<p>IDEALES: Honor. Soy un firme seguidor de mi fe, que me lleva por el camino del honor y la benevolencia..</p>
-							</div>
-							<div className='mb-2'>
-								<p>VÍNCULO: He sido testigo de innumerables crímenes, uno de ellos causando la muerte de mi hermano pequeño a sus 6 años. No toleraré que siga habiendo injusticias así en el mundo.</p>
-							</div>
-							<div className='mb-2'>
-								<p>DEFECTO: Mi obsesión por un mundo ideal a veces me lleva a tomar malas decisiones sin darme cuenta. Y ni hablemos de esa estúpida maldición...</p>
+							<div className="flex flex-col gap-2">
+								<Card>
+									<CardHeader>
+										<CardTitle>Rasgos de Personalidad</CardTitle>
+									</CardHeader>
+									<CardContent>
+										<p>Inquebrantable. "Me mantengo firme en mis principios, guiado siempre por la ley y la justicia."</p>
+									</CardContent>
+								</Card>
+								<Card>
+									<CardHeader>
+										<CardTitle>Ideales</CardTitle>
+									</CardHeader>
+									<CardContent>
+										<p>Responsabilidad: "Es mi deber proteger a los inocentes y luchar contra la injusticia en cualquier rincón del mundo."</p>
+									</CardContent>
+								</Card>
+								<Card>
+									<CardHeader>
+										<CardTitle>Vínculo</CardTitle>
+									</CardHeader>
+									<CardContent>
+										<p>Devoción: "Mi fe me guía por el buen camino. Debo salvar a todos aquellos que necesiten ser salvados."</p>
+									</CardContent>
+								</Card>
+								<Card>
+									<CardHeader>
+										<CardTitle>Defecto</CardTitle>
+									</CardHeader>
+									<CardContent>
+										<p>Autoexigencia: "Me impongo estándares demasiado altos, hasta el punto de cuestionar mi valía por cualquier error."</p>
+									</CardContent>
+								</Card>
 							</div>
 						</CardContent>
 					</Card>
@@ -79,7 +103,27 @@ export default function CharacterInfoPage() {
 							<CardSubtitle>Rasgos y Características</CardSubtitle>
 						</CardHeader>
 						<CardContent>
-							<p>(Contenido pendiente de cumplimentar)</p>
+						<Card className='mb-2'>
+								<CardHeader>
+									<CardTitle>Deidad Adorada</CardTitle>
+								</CardHeader>
+								<CardContent>
+									<p>
+										Torm, el dios del coraje y el sacrificio.
+									</p>
+								</CardContent>
+							</Card>
+							<Card>
+								<CardHeader>
+									<CardTitle>Sentido Divino</CardTitle>
+									<CardSubtitle>1 acción | Usos: 1 + CAR (recupera en DL)</CardSubtitle>
+								</CardHeader>
+								<CardContent>
+									<p>
+										Detectas la ubicación de celestiales, demonios y no-muertos, además de objetos consagrados o profanados, en un radio de 60 pies.
+									</p>
+								</CardContent>
+							</Card>
 						</CardContent>
 					</Card>
 				)
