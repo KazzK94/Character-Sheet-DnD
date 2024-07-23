6fec8ef0f8165b02e380714bc3bf56b014369905
Kazz <i94773007@gmail.com>
2024-07-23 14:34:29 +0200

    added animation to secondary navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,33 @@ export default {
 		extend: {
 			colors: {
 				"primary": "rgb(20, 36, 80)"
+			},
+			animation: {
+				secondaryMenuSlideUp: 'secondaryMenuSlideUp 0.3s ease-in-out',
+				secondaryMenuSlideDown: 'secondaryMenuSlideDown 0.3s ease-in-out'
+			},
+			keyframes: {
+				secondaryMenuSlideUp: {
+					'0%': {
+						transform: 'translateY(4rem)',
+						opacity: 0.2
+					},
+					'100%': {
+						transform: 'translateY(0)',
+						opacity: 1
+					},
+				},
+				secondaryMenuSlideDown: {
+					'0%': {
+						transform: 'translateY(-4rem)',
+						opacity: 0.2
+					},
+					'100%': {
+						transform: 'translateY(0)',
+						opacity: 1
+					},
+				},
+
 			}
 		},
 	},
